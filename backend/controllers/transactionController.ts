4231d328632689b419571c0029908960adfeaf6e
Kedar Basutkar <kedar-int@sharefable.com>
2022-12-02 23:56:24 +0530

    add: add transaction controller & route

--- backend/controllers/transactionController.ts
+++ backend/controllers/transactionController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { connection } from '../config/database'
-import { GET_ALL_TRANSACTIONS } from '../services/transactionServices'
+import { GET_ALL_TRANSACTIONS, INSERT_TRANSACTION } from '../services/transactionServices'
 import { internalServerError } from '../utils/internalServerError'
 
 export const getAllTransactions = (req: Request, res: Response): Response | undefined => {
@@ -15,6 +15,41 @@ export const getAllTransactions = (req: Request, res: Response): Response | unde
           success: true,
           message: 'Transactions fetched successfully.',
           transactions: results
+        })
+      }
+    )
+  } catch (err) {
+    return internalServerError(res)
+  }
+}
+
+interface Transaction {
+  name: string
+  amount: number
+  category: string
+  timestamp?: string
+}
+
+export const addTransaction = (req: Request, res: Response): Response | undefined => {
+  const { name, amount, category, timestamp }: Transaction = req.body
+
+  if (name === undefined || amount === undefined || category === undefined || timestamp === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'Send transaction name, amount, category & timestamp'
+    })
+  }
+
+  try {
+    connection.query(
+      INSERT_TRANSACTION, [name, amount, category, timestamp],
+      (err, results) => {
+        if (err !== null) {
+          return internalServerError(res)
+        }
+        return res.status(200).json({
+          success: true,
+          message: 'Transactions added successfully.'
         })
       }
     )
--- backend/routes/transaction.ts
+++ backend/routes/transaction.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getAllTransactions } from '../controllers/transactionController'
+import { getAllTransactions, addTransaction } from '../controllers/transactionController'
 
 const router = express.Router()
 
 router.route('/getAllTransactions').get(getAllTransactions)
+router.route('/transaction').post(addTransaction)
 
 export default router
