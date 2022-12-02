a60b6d13ab168ff81727e69cd76931289e73f87e
Kedar Basutkar <kedar-int@sharefable.com>
2022-12-03 00:03:20 +0530

    add: delete query

--- backend/services/transactionServices.ts
+++ backend/services/transactionServices.ts
@@ -1,4 +1,7 @@
 export const GET_ALL_TRANSACTIONS = 'SELECT * FROM transactions;'
+
 export const INSERT_TRANSACTION = `INSERT INTO transactions 
   (name, amount, category, timestamp) 
   VALUES (?, ?, ?, ?);`
+
+export const DELETE_TRANSACTION = 'DELETE FROM transactions WHERE id = ?;'
