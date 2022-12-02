a86fa057dbefba9b9e0e83a54a7a7d6e720da5bb
Kedar Basutkar <kedar-int@sharefable.com>
2022-12-03 00:54:18 +0530

    setup: tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 function App(): JSX.Element {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+  return <h1 className="text-sm font-bold underline">Hello World</h1>;
 }
 
 export default App;
