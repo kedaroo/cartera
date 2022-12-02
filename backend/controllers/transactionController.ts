6f77a6494dfb578a98402a9bc5c1a91de5b03bfa
Kedar Basutkar <kedar-int@sharefable.com>
2022-12-03 00:03:42 +0530

    feat: delete transaction controller & route

--- backend/controllers/transactionController.ts
+++ backend/controllers/transactionController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { connection } from '../config/database'
-import { GET_ALL_TRANSACTIONS, INSERT_TRANSACTION } from '../services/transactionServices'
+import { GET_ALL_TRANSACTIONS, INSERT_TRANSACTION, DELETE_TRANSACTION } from '../services/transactionServices'
 import { internalServerError } from '../utils/internalServerError'
 
 export const getAllTransactions = (req: Request, res: Response): Response | undefined => {
@@ -50,6 +50,34 @@ export const addTransaction = (req: Request, res: Response): Response | undefine
         return res.status(200).json({
           success: true,
           message: 'Transactions added successfully.'
+        })
+      }
+    )
+  } catch (err) {
+    return internalServerError(res)
+  }
+}
+
+export const deleteTransaction = (req: Request, res: Response): Response | undefined => {
+  const id: number = req.body.id
+
+  if (id === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'Send transaction id'
+    })
+  }
+
+  try {
+    connection.query(
+      DELETE_TRANSACTION, [id],
+      (err, results) => {
+        if (err !== null) {
+          return internalServerError(res)
+        }
+        return res.status(200).json({
+          success: true,
+          message: 'Transactions deleted successfully.'
         })
       }
     )
--- backend/routes/transaction.ts
+++ backend/routes/transaction.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import { getAllTransactions, addTransaction } from '../controllers/transactionController'
+import { getAllTransactions, addTransaction, deleteTransaction } from '../controllers/transactionController'
 
 const router = express.Router()
 
 router.route('/getAllTransactions').get(getAllTransactions)
-router.route('/transaction').post(addTransaction)
+router.route('/transaction').post(addTransaction).delete(deleteTransaction)
 
 export default router
