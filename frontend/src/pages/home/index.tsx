cd670275c87946fc87ed6aed6818ca8530af570a
Kedar Basutkar <kedar-int@sharefable.com>
2022-12-03 04:02:44 +0530

    add: Home page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,3 +1,15 @@
+import TopGreeting from "../../components/pages/TopGreeting";
+import BalanceCard from "../../components/pages/balance_card";
+import TransactionList from "../../components/pages/transaction_list";
+
 export default function Home() {
-  return <div>Home Screen</div>;
+  return (
+    <div className="bg-gray-100">
+      <div className="mx-auto min-h-screen max-w-md py-12 px-6">
+        <TopGreeting />
+        <BalanceCard balance={10000} income={7000} expense={2000} />
+        <TransactionList />
+      </div>
+    </div>
+  );
 }
