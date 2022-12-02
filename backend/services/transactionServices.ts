6916f5f38df5fc561d3525f377d30e934985eb3f
Kedar Basutkar <kedar-int@sharefable.com>
2022-12-02 23:41:07 +0530

    add: insert transaction query

--- backend/services/transactionServices.ts
+++ backend/services/transactionServices.ts
@@ -1,1 +1,4 @@
 export const GET_ALL_TRANSACTIONS = 'SELECT * FROM transactions;'
+export const INSERT_TRANSACTION = `INSERT INTO transactions 
+  (name, amount, category, created_at) 
+  VALUES (?, ?, ?, ?);`
